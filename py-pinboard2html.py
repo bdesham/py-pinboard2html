a8fe483e5fdd37a1071ff0f861bb4633912c4d7c
Benjamin Esham <benjamin@esham.io>
2024-07-21 13:06:35 -0400

    Remove trailing whitespace and convert tabs to spaces

--- py-pinboard2html.py
+++ py-pinboard2html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # py-pinboard2html
-# 
+#
 # Convert your Pinboard bookmarks into an HTML bookmarks file
 #
 # Copyright (c) 2011-2012 Benjamin D. Esham. This program is released under the
@@ -29,25 +29,25 @@ token = "2AB96390C7DBE34"
 script_version = "1.3"
 
 def version_text():
-	old_out = sys.stdout
-	sys.stdout = sys.stderr
+    old_out = sys.stdout
+    sys.stdout = sys.stderr
 
-	print "py-pinboard2html", script_version
-	print "(c) 2011-12, Benjamin Esham"
-	print "https://github.com/bdesham/py-pinboard2html"
+    print "py-pinboard2html", script_version
+    print "(c) 2011-12, Benjamin Esham"
+    print "https://github.com/bdesham/py-pinboard2html"
 
-	sys.stdout = old_out
+    sys.stdout = old_out
 
 def help_text():
-	version_text()
+    version_text()
 
-	old_out = sys.stdout
-	sys.stdout = sys.stderr
+    old_out = sys.stdout
+    sys.stdout = sys.stderr
 
-	print
-	print "usage: python py-pinboard2html.py [-u username -t token] [output-file]"
+    print
+    print "usage: python py-pinboard2html.py [-u username -t token] [output-file]"
 
-	sys.stdout = old_out
+    sys.stdout = old_out
 
 
 # functions
@@ -55,34 +55,34 @@ def help_text():
 # html escaping code from http://wiki.python.org/moin/EscapingHtml
 
 html_escape_table = {
-	"&": "&amp;",
-	'"': "&quot;",
-	"'": "&#39;",
-	">": "&gt;",
-	"<": "&lt;",
-	}
+    "&": "&amp;",
+    '"': "&quot;",
+    "'": "&#39;",
+    ">": "&gt;",
+    "<": "&lt;",
+    }
 
 def html_escape(text):
-	return ''.join(html_escape_table.get(c, c) for c in text)
+    return ''.join(html_escape_table.get(c, c) for c in text)
 
 def sanitize(string):
-	res = ''
-	string = html_escape(string)
+    res = ''
+    string = html_escape(string)
 
-	for i in range(len(string)):
-		if ord(string[i]) > 127:
-			res += '&#x%x;' % ord(string[i])
-		else:
-			res += string[i]
+    for i in range(len(string)):
+        if ord(string[i]) > 127:
+            res += '&#x%x;' % ord(string[i])
+        else:
+            res += string[i]
 
-	return res
+    return res
 
 def html_bookmarks(bookmarks):
-	return ''.join(html_bookmark(b) for b in bookmarks)
+    return ''.join(html_bookmark(b) for b in bookmarks)
 
 def html_bookmark(bookmark):
-	return "<dt><a href=\"%s\">%s</a></dt>\n" % \
-			(sanitize(bookmark['href']), sanitize(bookmark['description']))
+    return "<dt><a href=\"%s\">%s</a></dt>\n" % \
+            (sanitize(bookmark['href']), sanitize(bookmark['description']))
 
 
 # parse command-line arguments
@@ -90,44 +90,44 @@ def html_bookmark(bookmark):
 args = sys.argv[1:]
 
 if "-v" in args or "--version" in args:
-	version_text()
-	sys.exit(1)
+    version_text()
+    sys.exit(1)
 
 if len(args) not in [0, 1, 4, 5] or "-h" in args or "--help" in args:
-	help_text()
-	sys.exit(2)
+    help_text()
+    sys.exit(2)
 
 if "-u" in args and "-t" in args:
-	ind = args.index("-u")
+    ind = args.index("-u")
 
-	try:
-		username = args[ind+1]
-	except IndexError, e:
-		help_text()
-		sys.exit(2)
+    try:
+        username = args[ind+1]
+    except IndexError, e:
+        help_text()
+        sys.exit(2)
 
-	args.pop(ind)
-	args.pop(ind)
-	
-	try:
-		# wrap this in a try block in case we got something like "-u -t"
-		ind = args.index("-t")
-	except ValueError, e:
-		help_text()
-		sys.exit(2)
+    args.pop(ind)
+    args.pop(ind)
 
-	try:
-		token = args[ind+1]
-	except IndexError, e:
-		help_text()
-		sys.exit(2)
-	
-	args.pop(ind)
-	args.pop(ind)
+    try:
+        # wrap this in a try block in case we got something like "-u -t"
+        ind = args.index("-t")
+    except ValueError, e:
+        help_text()
+        sys.exit(2)
+
+    try:
+        token = args[ind+1]
+    except IndexError, e:
+        help_text()
+        sys.exit(2)
+
+    args.pop(ind)
+    args.pop(ind)
 elif "-u" in args or "-t" in args:
-	# user gave a username but not a token, or vice versa
-	help_text()
-	sys.exit(2)
+    # user gave a username but not a token, or vice versa
+    help_text()
+    sys.exit(2)
 
 # get the data from pinboard
 
@@ -136,30 +136,30 @@ pipe = subprocess.Popen(["curl", "-s", url], stdout = subprocess.PIPE)
 data = pipe.communicate()[0]
 
 if len(data) == 0:
-	print >> sys.stderr, "py-pinboard2html: Pinboard response was empty."
-	sys.exit(1)
+    print >> sys.stderr, "py-pinboard2html: Pinboard response was empty."
+    sys.exit(1)
 
 try:
-	bookmarks = json.loads(data)
+    bookmarks = json.loads(data)
 except ValueError, e:
-	print >> sys.stderr, "py-pinboard2html: error parsing JSON. " + \
-			"Did you enter the right API token?"
-	print >> sys.stderr, e
-	sys.exit(1)
+    print >> sys.stderr, "py-pinboard2html: error parsing JSON. " + \
+            "Did you enter the right API token?"
+    print >> sys.stderr, e
+    sys.exit(1)
 
 # write out the html file
 
 if len(args):
-	try:
-		out = open(os.path.expanduser(args[0]), 'w')
-	except IOError, e:
-		print >> sys.stderr, "py-pinboard2html: error opening the output file."
-		print >> sys.stderr, e
-		sys.exit(1)
-	output_to_file = True
+    try:
+        out = open(os.path.expanduser(args[0]), 'w')
+    except IOError, e:
+        print >> sys.stderr, "py-pinboard2html: error opening the output file."
+        print >> sys.stderr, e
+        sys.exit(1)
+    output_to_file = True
 else:
-	out = sys.stdout
-	output_to_file = False
+    out = sys.stdout
+    output_to_file = False
 
 print >> out, '<html xmlns:NC="http://home.netscape.com/NC-rdf#">'
 print >> out, '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>'
@@ -169,4 +169,4 @@ print >> out, '</html>'
 # clean up
 
 if output_to_file:
-	out.close()
+    out.close()
